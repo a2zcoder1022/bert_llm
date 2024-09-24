e0c9e2f2b011f9298baeb021efe818baa9013492
a2zcoder1022 <a2zdeveloper1234@gmail.com>
2024-09-24 16:16:21 -0400

    Update BertModel.py

--- BertModel.py
+++ BertModel.py
@@ -2,3 +2,14 @@ import torch.nn as nn
 class BertModel(nn.Module):
     def __init__(self):
         pass
+
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        pass
+
+
+class Decoder(nn.Module):
+    def __init__(self):
+        pass
